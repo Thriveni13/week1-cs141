ccb9051b14d1d3b1e59fad17ca7d70b1bcc2c92e
Harshit Sharma <harshit.sharma@niser.ac.in>
2019-08-08 10:03:23 -0400

    finished

---
+++ q1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "What is your name?"
+read name
+mkdir $name
+touch $name/$name.cpp
+
+
---
+++ q2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo " Hello, what is yous name? "
+read name
+mkdir $name
+cd $name
+touch $name"1".cpp
+touch $name"2".cpp
+touch $name"3".cpp
+touch $name"4".cpp
+touch $name"5".cpp
+
